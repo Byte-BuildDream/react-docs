a7c48687d4405edc9fe5df3e0c9819dd80cd87ae
MrZhuxs <zhuxsnb@gmail.com>
2025-06-07 16:28:59 +0800

    [language]：更新布局组件以支持国际化，添加中文和英文语言选项；修改文档元数据以反映新的标题和描述

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,33 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+      <RootProvider
+        i18n={{
+          locale: 'cn',
+          locales: [
+            {
+              name: 'English',
+              locale: 'en',
+            },
+            {
+              name: 'Chinese',
+              locale: 'cn',
+            },
+          ],
+          translations: {
+            toc: '本页内容',
+            search: '搜索文档',
+            lastUpdate: '最后更新于',
+            searchNoResult: '未查找到结果',
+            previousPage: '上一页',
+            nextPage: '下一页',
+            chooseLanguage: '选择语言',
+            chooseTheme: '选择主题',
+          }
+        }}
+      >
+        {children}
+      </RootProvider>
       </body>
     </html>
   );
